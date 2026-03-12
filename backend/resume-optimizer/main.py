fd4c2518378e4f63cafc836a24cf5e41304aa2fb
owolabi-develop <owolabidevelop84@gmail.com>
2026-03-12 07:15:55 -0700

    add resume file extraction utile function

--- backend/resume-optimizer/main.py
+++ backend/resume-optimizer/main.py
@@ -1,14 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks,UploadFile,Form,File
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
+from utils.document_processing import ProcessDocument
 
 app = FastAPI(title="Resume Optimizer",
               summary="Advance resume optimization system for ATS (Application Tracking system)")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# CORS (Cross-Origin Resource Sharing) config
+origins = [
+    "http://localhost:5173/",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/resume/optimize/")
+async def optimize_resume(resume:UploadFile,job_description: Annotated[str, Form()]):
+    # process resume file
+    if resume.content_type =="application/pdf":
+        print(f"processing resume: {resume.filename} type: PDF")
+        contents = await resume.read()
+        # extract text content from pdf document
+        extract_text = ProcessDocument(contents).process_pdf_file()
+        
+    else:
+        print(f"processing resume: {resume.filename} type: Docs")
+        contents = await resume.read()
+         # extract text content from doc document
+        extract_text = ProcessDocument(contents).process_docx_file()
+    
+    return {"filename":resume.file}
---
+++ backend/resume-optimizer/utils/document_processing.py
@@ -0,0 +1,32 @@
+import pymupdf
+from docx import Document
+from io import BytesIO
+
+class ProcessDocument:
+    def __init__(self,file):
+        self.file = file
+
+    def process_pdf_file(self):
+        doc = pymupdf.open(stream=self.file,filetype="pdf")
+        fullText = ""
+        for page in doc:
+            text = page.get_text()
+            fullText += text
+        return fullText.replace(" ","")
+        
+
+
+
+
+    def process_docx_file(self):
+         source_stream = BytesIO(self.file)
+         doc = Document(source_stream)
+        
+         fullText = []
+
+         for para in doc.paragraphs:
+            print(para)
+            fullText.append(para.text.replace(" ",""))
+         return '\n'.join(fullText) 
+
+         
